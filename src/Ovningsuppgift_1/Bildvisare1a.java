c013bd8d5c658e35a2a8111296b8af16e6dae50f
IvonaZoricic <ivona.zoricic@gmail.com>
2020-10-19 14:49:43 +0200

    Decreased image sizes

--- src/Ovningsuppgift_1/Bildvisare1a.java
+++ src/Ovningsuppgift_1/Bildvisare1a.java
@@ -3,26 +3,19 @@ package Ovningsuppgift_1;
 import javax.swing.*;
 import java.awt.*;
 
-/**
- * Created by Ivona Zoricic
- * Date: 2020-10-19
- * Time: 12:23
- * Project: Sprint3
- * Copywrite: MIT
- */
 public class Bildvisare1a extends JFrame {
 
     JPanel p;
     JButton b;
     JLabel l;
-    String getPath = "src/Ovningsuppgift_1/Bilder/flower.jpg";
+    String getPath = "src/Bilder/flower.jpg";
 
     Bildvisare1a(){
-        p = new JPanel(new GridLayout(2,1));
+        p = new JPanel(new FlowLayout());
         b = new JButton("Växla bild");
         l = new JLabel(new ImageIcon(getPath));
-        p.add(b);
         p.add(l);
+        p.add(b);
         this.add(p);
         this.pack();
         this.setLocation(500, 500);
